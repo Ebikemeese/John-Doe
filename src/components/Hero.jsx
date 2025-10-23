d87b0f4869400351e612ddf56c276b3a551ad09c
Ebikeme <ebikemeese257@gmail.com>
2025-10-23 08:15:32 +0100

    Pre-deploy

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,12 @@
 import { motion } from "framer-motion";
-import { Link } from 'react-router-dom'
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
     return (
-        <section className={`relative w-full h-screen mx-auto`}>
+        <section classNam="relative w-full h-screen mx-auto">
             <div
-                className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+                className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
             >
                 <div className='flex flex-col justify-center items-center mt-5'>
                     <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
